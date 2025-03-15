40a6104496467b4247e21758c5877131dd04fcd3
Sreeja <86891472+sreeja-sen@users.noreply.github.com>
2025-03-15 11:40:16 +0000

    finished initial page structure

--- src/App.js
+++ src/App.js
@@ -3,26 +3,55 @@ import "./App.css";
 function App() {
     return (
         <div className="app">
-            <header className="header"></header>
+            <header className="header">
+                <div className="header-left">
+                    <button>Full catalog</button>
+                    <button>Request a book</button>
+                    <button>Donate a book</button>
+                </div>
+                <div className="header-right">
+                    <button>Account</button>
+                </div>
+            </header>
             <div className="content">
-                <aside className="search-bar">
-                    <input type="text" className="search-input" />
-                    <button className="search-button"></button>
-                </aside>
-                <main className="main">
-                    <div className="button-group">
-                        <button className="button"></button>
-                        <button className="button"></button>
-                        <button className="button"></button>
-                        <button className="button"></button>
-                        <button className="button"></button>
-                    </div>
-                    <div className="books">
-                        <article className="book"></article>
-                        <article className="book"></article>
-                        <article className="book"></article>
-                    </div>
-                </main>
+                <div className="content-wrapper">
+                    <aside className="search-bar">
+                        <input type="text" className="search-input" placeholder="..."/>
+                        <button className="search-button">Search</button>
+                    </aside>
+                    <main className="main">
+                        <div className="button-group">
+                            <button className="button">A</button>
+                            <button className="button">B</button>
+                            <button className="button">C</button>
+                            <button className="button">D</button>
+                            <button className="button">E</button>
+                        </div>
+                        <div className="books">
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                            <article className="book"></article>
+                        </div>
+                    </main>
+                </div>
             </div>
         </div>
     );
