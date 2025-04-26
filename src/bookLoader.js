6e03e7c3717bedccdbe96855a504809ebd2f55d1
Sreeja <86891472+sreeja-sen@users.noreply.github.com>
2025-04-26 10:04:13 +0000

    Implemented book transformer middleware and managed book stateful book display

--- src/bookLoader.js
+++ src/bookLoader.js
@@ -5,12 +5,24 @@
 //     return data;
 // }
 
+// TODO: Formalise definition of middleware for book loader
 function bookLoader() {
     const promise = new Promise((success, error) => fetch("books.json")
         .then(response => response.json())
-        .then(data => success(data)))
+        .then(data => {
+            data = data.map(bookDataTransformer);
+            success(data);
+        }))
 
     return promise;
 }
+
+function bookDataTransformer(bookJson) {
+    const coverImgRegex = /^.*\/(.*\/.*\.(png|jpg))$/;
+    const matches = bookJson.coverImg.match(coverImgRegex);
+    const coverImgLocal = "covers/" + matches[1].replace("/", "");
+    
+    return { ...bookJson, coverImgLocal };
+  }
 
 export default bookLoader;
